cf51a7a435d95caacecb0062b8a78a786dd8bca4
AgusBellas <bag_94_11@hotmail.com>
2018-11-18 18:42:13 +0100

    Añadidos dialogos y activities que faltaban (modificar, preferencias,...). Reajuste de funciones para CrearRecetas y para su layout. Mas modificaciones.

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f090029;
-        public static final int end = 0x7f09004c;
-        public static final int gone = 0x7f09005a;
-        public static final int invisible = 0x7f090069;
-        public static final int left = 0x7f09007a;
-        public static final int packed = 0x7f090097;
-        public static final int parent = 0x7f090099;
-        public static final int percent = 0x7f09009c;
-        public static final int right = 0x7f0900a2;
-        public static final int spread = 0x7f0900c5;
-        public static final int spread_inside = 0x7f0900c6;
-        public static final int start = 0x7f0900cb;
-        public static final int top = 0x7f0900e0;
-        public static final int wrap = 0x7f0900f7;
+        public static final int end = 0x7f09004d;
+        public static final int gone = 0x7f09005b;
+        public static final int invisible = 0x7f09006a;
+        public static final int left = 0x7f09007b;
+        public static final int packed = 0x7f090098;
+        public static final int parent = 0x7f09009a;
+        public static final int percent = 0x7f09009d;
+        public static final int right = 0x7f0900a3;
+        public static final int spread = 0x7f0900c6;
+        public static final int spread_inside = 0x7f0900c7;
+        public static final int start = 0x7f0900cc;
+        public static final int top = 0x7f0900e1;
+        public static final int wrap = 0x7f0900f8;
     }
     public static final class styleable {
         private styleable() {}
